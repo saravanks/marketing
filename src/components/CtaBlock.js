b23ef1381bfc638835ff7b5701785fa53b470a37
Yev Krupetsky <yevk@altostra.com>
2019-08-26 16:19:35 +0300

    Replace JOIN buttons with new ones for Join dialog

--- src/components/CtaBlock.js
+++ src/components/CtaBlock.js
@@ -2,6 +2,7 @@ import React from 'react';
 import _ from 'lodash';
 
 import {htmlToReact, Link, safePrefix} from '../utils';
+import JoinDialog from '../components/JoinDialog';
 
 export default class CtaBlock extends React.Component {
     render() {
@@ -20,9 +21,10 @@ export default class CtaBlock extends React.Component {
                 </div>
                 {_.get(this.props, 'section.actions') &&
                 <div class="cell block-buttons">
-                    {_.map(_.get(this.props, 'section.actions'), (action, action_idx) => (
+                    {/* {_.map(_.get(this.props, 'section.actions'), (action, action_idx) => (
                     <Link key={action_idx} to={safePrefix(_.get(action, 'url'))} class="button">{_.get(action, 'label')}</Link>
-                ))}
+                ))} */}
+                <JoinDialog buttonText="Create your first project" />
                 </div>
                 }
               </div>
--- src/components/HeroBlock.js
+++ src/components/HeroBlock.js
@@ -2,6 +2,7 @@ import React from 'react';
 import _ from 'lodash';
 
 import {safePrefix, markdownify, Link} from '../utils';
+import JoinDialog from '../components/JoinDialog';
 
 export default class HeroBlock extends React.Component {
     render() {
@@ -20,18 +21,20 @@ export default class HeroBlock extends React.Component {
                       {markdownify(_.get(this.props, 'section.content'))}
                     </div>
                     {_.get(this.props, 'section.actions') && 
-                    <p class="block-buttons">
-                      {_.map(_.get(this.props, 'section.actions'), (action, action_idx) => (
-                      <Link key={action_idx} to={safePrefix(_.get(action, 'url'))} class="button white large">{_.get(action, 'label')}</Link>
-                      ))}
-                    </p>
+                    <JoinDialog buttonText="JOIN BETA" buttonColor="white" buttonSize="large" />
+                    // <p class="block-buttons">
+                    //   {_.map(_.get(this.props, 'section.actions'), (action, action_idx) => (
+                    //   <Link key={action_idx} to={safePrefix(_.get(action, 'url'))} class="button white large">{_.get(action, 'label')}</Link>
+                    //   ))}
+                      
+                    // </p>
                     }
                   </div>
                 </div>
               </div>
                 <div class="wave" style={{position:'relative', bottom:'-10px'}} aria-hidden="true">
                     <svg width="2060" height="187" viewBox="0 0 2060 187" xmlns='http://www.w3.org/2000/svg'>
-                        <g fill="none" fill-rule="evenodd">
+                        <g fill="none" fillRule="evenodd">
                             <path class="path1" d="M2060 0v62.124C1030 62.124 1030 177 0 177v-6.544C1060.445 177 1009.883 0 2060 0z" fill="#E5E6FF"></path>
                             <path class="path2" d="M2060 186.859H0v-11.047C1030 175.812 1071.994 29.39 2060 61v125.859z" fill="#f7f9fb"></path>
                         </g>
