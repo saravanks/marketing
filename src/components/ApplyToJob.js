68a5cadcfb45570307f9db31f76195f28685eb68
Yev Krupetsky <yevk@altostra.com>
2019-09-10 17:59:33 +0300

    Make recaptcha invisible in ApplyToJob dialog

--- src/components/ApplyToJob.js
+++ src/components/ApplyToJob.js
@@ -5,92 +5,100 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Recaptcha from 'react-recaptcha';
 
-export default function JoinDialog({ buttonText, job, buttonSize = '', buttonColor='' }) {
-  const [open, setOpen] = React.useState(false);
-  const [insurance, setInsurance] = React.useState('');
-  const [linkedInUrl, setLinkedInUrl] = React.useState('');
-  const [recaptcha, setRecaptcha] = React.useState('')
-  const [validSubmitAttempted, setValidSubmitAttempted] = React.useState(false)
-  const form = React.createRef();
+export default class JoinDialog extends React.Component {
+  constructor(props) {
+    super(props);
 
-  function handleClickOpen() {
-    setOpen(true);
+    this.state = {
+      insurance: '',
+      linkedInUrl: '',
+      open: false
+    };
+
+    this.form = React.createRef();
+    this.recaptchaEl = React.createRef();
   }
 
-  function handleClose() {
-    setOpen(false);
-    setLinkedInUrl('');
-    setRecaptcha('');
-    setValidSubmitAttempted(false);
+  handleClickOpen() {
+    this.setState({ open: true });
   }
 
-  function handleSubmit() {
-    if (form.current.reportValidity()) {
-      setValidSubmitAttempted(true);
-
-      if (recaptcha) {
-        fetch('https://at8732o4l6.execute-api.us-east-1.amazonaws.com/Prod/apply', {
-          method: 'POST',
-          mode: 'cors',
-          headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/vnd.altostra+json;version=1.0'
-          },
-          cache: 'no-cache',
-          body: JSON.stringify({
-            url: linkedInUrl,
-            job,
-            insurance,
-            captcha: recaptcha
-          })
-        })
-
-        handleClose()
-      }
+  handleSubmit() {
+    if (this.form.current.reportValidity()) {
+      this.recaptchaEl.current.execute();
     }
   }
 
-  return (
-    <div className="apply-to-job">
-      <button className={`button ${buttonSize} ${buttonColor}`} onClick={handleClickOpen}>
-      {buttonText}
-      </button>
+  handleClose() {
+    this.setState({
+      open: false,
+      linkedInUrl: ''
+    })
+  }
 
-      <Dialog scroll="body" open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Join Us</DialogTitle>
-        <DialogContent>
-            <form autoComplete="on" ref={form} name="joinForm" action="#" id="join-form" className="join-form">
-              <p className="screen-reader-text">
-                <label>Don't fill this out if you're human: <input id="join-insurance" name="insurance-field" onChange={e => setInsurance(e.target.value)} value={insurance} /></label>
-              </p>
-              
-              <p className="form-row">
-                <label className="form-label">LinkedIn URL</label>
-                <input
-                  className="form-input"
-                  name="linkedinurl"
-                  type="url"
-                  autoComplete="url"
-                  required
-                  autoFocus
-                  onChange={e => setLinkedInUrl(e.target.value)}
-                  value={linkedInUrl}
+  handleRecaptchaVerify(captcha) {
+    fetch('https://at8732o4l6.execute-api.us-east-1.amazonaws.com/Prod/apply', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/vnd.altostra+json;version=1.0'
+      },
+      cache: 'no-cache',
+      body: JSON.stringify({
+        url: this.state.linkedInUrl,
+        job: this.props.job,
+        insurance: this.state.insurance,
+        captcha
+      })
+    })
+
+    this.handleClose()
+  }
+
+  render() {
+    return (
+      <div className="apply-to-job">
+        <button className={`button ${this.props.buttonSize} ${this.props.buttonColor}`} onClick={this.handleClickOpen.bind(this)}>
+        {this.props.buttonText}
+        </button>
+  
+        <Dialog scroll="body" open={this.state.open} onClose={this.handleClose.bind(this)} aria-labelledby="form-dialog-title">
+          <DialogTitle id="form-dialog-title">Join Us</DialogTitle>
+          <DialogContent>
+              <form autoComplete="on" ref={this.form} name="joinForm" action="#" id="join-form" className="join-form">
+                <p className="screen-reader-text">
+                  <label>Don't fill this out if you're human: <input id="join-insurance" name="insurance-field" onChange={e => this.setState({ insurance: e.target.value })} value={this.state.insurance} /></label>
+                </p>
+                
+                <p className="form-row">
+                  <label className="form-label">LinkedIn URL</label>
+                  <input
+                    className="form-input"
+                    name="linkedinurl"
+                    type="url"
+                    autoComplete="url"
+                    required
+                    autoFocus
+                    onChange={e => this.setState({ linkedInUrl: e.target.value })}
+                    value={this.state.linkedInUrl}
+                  />
+                </p>
+  
+                <Recaptcha
+                  className="recaptcha fix-height"
+                  size="invisible"
+                  sitekey="6LdborcUAAAAAMf5FWf4aRv-UcUGWmaIU29Abf1u"
+                  verifyCallback={this.handleRecaptchaVerify.bind(this)}
+                  ref={this.recaptchaEl}
                 />
-              </p>
-
-              <Recaptcha
-                className={`recaptcha fix-height ${validSubmitAttempted && !recaptcha ? 'required' : ''}`}
-                size="compact"
-                sitekey="6LcpwrQUAAAAACiIUAogkhK9N0Es4_wZAh2J7CYE"
-                verifyCallback={response => setRecaptcha(response)}
-                expiredCallbacd={() => setRecaptcha('')}
-              />
-            </form>
-        </DialogContent>
-        <DialogActions>
-          <button className="button" onClick={handleSubmit}>Submit</button>
-        </DialogActions>
-      </Dialog>
-    </div>
-  );
+              </form>
+          </DialogContent>
+          <DialogActions>
+            <button className="button" onClick={this.handleSubmit.bind(this)}>Submit</button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
 }
